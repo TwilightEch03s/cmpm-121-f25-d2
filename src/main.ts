59b6413f506dcac997e28215f10145e468768164
Lorenzo Uk <103523532+TwilightEch03s@users.noreply.github.com>
2025-10-16 23:11:02 +0000

    Step #2 Complete! Added in marker drawing, click on canvas to draw if released or exiting the page, stop drawing

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,44 @@ canvas.width = 256;
 canvas.height = 256;
 canvas.className = "app-canvas";
 document.body.appendChild(canvas);
+
+// Setup drawing context
+const ctx = canvas.getContext("2d");
+
+//Check if ctx exists
+if (!ctx) {
+  throw new Error("Unable to get 2D context");
+}
+
+ctx.lineWidth = 2;
+ctx.lineCap = "round";
+ctx.strokeStyle = "black";
+
+// Set drawing handler
+let drawing = false;
+
+// Mouse clicked = draw
+canvas.addEventListener("mousedown", (event: MouseEvent) => {
+  drawing = true;
+  ctx.beginPath();
+  ctx.moveTo(event.offsetX, event.offsetY);
+});
+
+// Mouse move = draw line at mouse postion
+canvas.addEventListener("mousemove", (event: MouseEvent) => {
+  if (!drawing) {
+    return;
+  }
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+});
+
+// If mouse is released, stop drawing
+canvas.addEventListener("mouseup", () => {
+  drawing = false;
+});
+
+// If mouse exits canvas, stop drawing
+canvas.addEventListener("mouseleave", () => {
+  drawing = false;
+});
