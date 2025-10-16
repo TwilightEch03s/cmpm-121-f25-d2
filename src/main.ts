c25b51f25b28859f64d9749df058dcea6b95bd65
Lorenzo Uk <103523532+TwilightEch03s@users.noreply.github.com>
2025-10-16 22:56:27 +0000

    Step 1 Complete! Added in a h1 text saying "Sticker Sketchpad" and added the canvas with rounded borders that pop out.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+// Create title
+const title = document.createElement("h1");
+title.textContent = "Sticker Sketchpad";
+document.body.appendChild(title);
+
+// Create canvas
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+canvas.width = 256;
+canvas.height = 256;
+canvas.className = "app-canvas";
+document.body.appendChild(canvas);
