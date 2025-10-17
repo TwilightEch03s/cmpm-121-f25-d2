b6eb244283b6d61d849ae9a8d1971da4af87fb39
Lorenzo Uk <103523532+TwilightEch03s@users.noreply.github.com>
2025-10-17 00:43:57 +0000

    Step #3 Complete! Added in obsever for when the drawing that has been changed and everytiem you draw a line, the function redraw is called to redraw that line.

--- src/main.ts
+++ src/main.ts
@@ -28,11 +28,39 @@ ctx.strokeStyle = "black";
 // Set drawing handler
 let drawing = false;
 
+// Set up points
+const points: { x: number; y: number }[][] = [];
+let current_point: { x: number; y: number }[] = [];
+
+//Redraw lines
+function redraw() {
+  if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (const line of points) {
+      if (line.length == 0) {
+        continue;
+      }
+      ctx.beginPath();
+      ctx.moveTo(line[0].x, line[0].y);
+      for (let i = 1; i < line.length; i++) {
+        ctx.lineTo(line[i].x, line[i].y);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+// Observer for "drawing-changed"
+canvas.addEventListener("drawing-changed", () => {
+  redraw();
+});
+
 // Mouse clicked = draw
 canvas.addEventListener("mousedown", (event: MouseEvent) => {
   drawing = true;
-  ctx.beginPath();
-  ctx.moveTo(event.offsetX, event.offsetY);
+  current_point = [{ x: event.offsetX, y: event.offsetY }];
+  points.push(current_point);
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 // Mouse move = draw line at mouse postion
@@ -40,8 +68,8 @@ canvas.addEventListener("mousemove", (event: MouseEvent) => {
   if (!drawing) {
     return;
   }
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+  current_point.push({ x: event.offsetX, y: event.offsetY });
+  canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 // If mouse is released, stop drawing
